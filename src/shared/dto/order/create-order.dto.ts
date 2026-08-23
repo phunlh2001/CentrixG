648e974338b61460a10804484aeb41503f2d0f33
phunlh2001 <phunlh2001@gmail.com>
2026-08-23 21:16:44 +0700

    hot-fixed: create order without productId

--- src/shared/dto/order/create-order.dto.ts
+++ src/shared/dto/order/create-order.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, IsUUID } from 'class-validator';
 
 export class CreateOrderDto {
   @ApiProperty({ example: 100000, description: 'Order amount in VND' })
@@ -11,8 +11,6 @@ export class CreateOrderDto {
     example: 'aef481a7-97d9-4f65-a829-e10288d09d88',
     description: 'Target product ID for this order (required)',
   })
-  @IsNotEmpty()
-  @IsUUID()
-  @IsString()
-  productId: string;
+  @IsOptional()
+  productId?: string | null;
 }
